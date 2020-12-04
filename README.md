85a4bbf142e767e51c70edf0637da50d119b5497
Matt Solomon <matt@mattsolomon.dev>
2020-12-04 09:33:59 -0800

    Initial setup using solidity-template

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# matching_contracts
+# Gitcoin Matching Contracts
+
+TODO
+
+## Usage
+
+### Pre Requisites
+
+Before running any command, make sure to install dependencies:
+
+```sh
+$ yarn install
+```
+
+### Compile
+
+Compile the smart contracts with Hardhat:
+
+```sh
+$ yarn compile
+```
+
+### TypeChain
+
+Compile the smart contracts and generate TypeChain artifacts:
+
+```sh
+$ yarn build
+```
+
+### Lint Solidity
+
+Lint the Solidity code:
+
+```sh
+$ yarn lint:sol
+```
+
+### Lint TypeScript
+
+Lint the TypeScript code:
+
+```sh
+$ yarn lint:ts
+```
+
+### Test
+
+Run the Mocha tests:
+
+```sh
+$ yarn test
+```
+
+### Coverage
+
+Generate the code coverage report:
+
+```sh
+$ yarn coverage
+```
+
+### Clean
+
+Delete the smart contract artifacts, the coverage reports and the Hardhat cache:
+
+```sh
+$ yarn clean
+```
+
+## Acknowledgements
+
+This project was built with Paul Berg's [solidity-template](https://github.com/paulrberg/solidity-template)
---
+++ test/Greeter.ts
@@ -0,0 +1,32 @@
+import { Signer } from '@ethersproject/abstract-signer';
+import { ethers, waffle } from 'hardhat';
+
+import GreeterArtifact from '../artifacts/contracts/Greeter.sol/Greeter.json';
+
+import { Accounts, Signers } from '../types';
+import { Greeter } from '../typechain/Greeter';
+import { shouldBehaveLikeGreeter } from './Greeter.behavior';
+
+const { deployContract } = waffle;
+
+describe('Unit tests', function () {
+  before(async function () {
+    this.accounts = {} as Accounts;
+    this.signers = {} as Signers;
+
+    const signers: Signer[] = await ethers.getSigners();
+    this.signers.admin = signers[0];
+    this.accounts.admin = await signers[0].getAddress();
+  });
+
+  describe('Greeter', function () {
+    beforeEach(async function () {
+      const greeting: string = 'Hello, world!';
+      this.greeter = (await deployContract(this.signers.admin, GreeterArtifact, [
+        greeting,
+      ])) as Greeter;
+    });
+
+    shouldBehaveLikeGreeter();
+  });
+});
